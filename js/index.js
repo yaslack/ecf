f05cd9459829c382802167869e3cd9c9583caaf1
Yassin <yassin.dridi@etu.u-bordeaux.fr>
2022-04-08 13:07:33 +0200

    navbar collapse

--- js/index.js
+++ js/index.js
@@ -1,17 +1,6 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+document.addEventListener("DOMContentLoaded", function(){
+  // add padding top to show content behind navbar
+  navbar_height = document.querySelector('.navbar').offsetHeight;
+  document.body.style.paddingTop = navbar_height + 'px';
+});
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    navbar.classList.add("fixed-top")
-  } else {
-    navbar.classList.remove("fixed-top");
-  }
-}
