0df58c730bca0a19511212a3d70b2b1414e824aa
Yassin <yassin.dridi@etu.u-bordeaux.fr>
2022-04-16 09:59:47 +0200

    add login and creation manager

---
+++ Hypnos.session.sql
@@ -0,0 +1,20 @@
+create table ACCOUNT
+(
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    Password VARCHAR(50)
+)
+
+
+
+select * From hotels
+select * From account
+
+alter TABLE hotels
+DROP COLUMN number
+
+
+UPDATE hotels
+set manager = 'Van Gelis'
+where number = 7
